9ea172f5251ae2281ab0b1d09d5d2e5262df487a
Jenifer Gonçalves da Silva <49965702+jeniferGoncalvesDaSilvaDev@users.noreply.github.com>
2025-04-28 21:12:29 -0300

    Update main.py

--- scents-cycor/scents/main.py
+++ scents-cycor/scents/main.py
@@ -485,7 +485,7 @@ def register():
         db.session.add(new_user)
         db.session.commit()
 
-        assunto = " Bem vindo a SCENTESIA - Tecnologia Scents by Sounds 
+        assunto = " Bem vindo a SCENTESIA - Tecnologia Scents by Sounds"
         corpo = f"""
 Olá, {data['nome']}!
 
